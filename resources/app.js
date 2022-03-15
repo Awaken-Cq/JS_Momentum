cf779291bc8854f5f77629f72c73b6771629f4b1
njtak <jeongtak@sp1soft.com>
2022-03-15 10:40:48 +0900

    바닐라 자바스크립트공부 진행

--- resources/app.js
+++ resources/app.js
@@ -1,24 +1,20 @@
-const title = document.querySelector("body > h1");
-console.log('title::', title);
-console.dir(title); // 해당 객체에 바인딩 할 수 있는 이벤트를 확인가능
+const formLogin = document.getElementById('formLogin');
+const inputLogin = document.querySelector("input");
+// *login button 으로 처리 시
+//const btnLogin = document.querySelector("button");
 
-function handleTitleClick(target) {
-  const _this = this;
-  console.log('title was clicked!');
-  console.log('target::', target);
+// btnLogin.addEventListener('click', function() {
+//   console.log('doLogin');
+//   console.log('input::', inputLogin.value);
+//   const inputValue = inputLogin.value;
 
-  this.style.color = 'blue';
-}
+// });
 
-title.addEventListener('click', handleTitleClick);
+// *submit으로 처리 시
+formLogin.addEventListener('submit', function() {
+  console.log('doLogin');
+  console.log('input::', inputLogin.value);
+  const inputValue = inputLogin.value;
 
-title.addEventListener('mouseenter', function() {
-  console.log('mouse is here');
-  title.innerText = 'Mouse is here';
-});
-
-title.addEventListener('mouseleave', function() {
-  console.log('mouse is leaved');
-  title.innerText = 'Mouse is gone';
 });
 
